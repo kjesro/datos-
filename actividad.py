309368e4f2c28ef8c7bcca7e30ebf198e52d0760
kjesro <127893735+kjesro@users.noreply.github.com>
2023-04-12 13:14:30 -0500

    ACTIVIDADES

---
+++ actividad.py
@@ -0,0 +1,17 @@
+#punto 1 
+"""
+inicio algoritmo
+    Mostrar "ingrese su peso"
+    leer peso
+    Mostrar "ingrese su edad"
+    leer edad
+    Mostrar " su peso es _peso_ y su edad es _edad_"
+  fin algoritmo
+"""
+
+print ("ingrese su peso")
+peso = input()
+print ("ingrese su edad")
+edad = input()
+print("su peso es" + peso + "kg y su edad es " + edad + " años")
+
